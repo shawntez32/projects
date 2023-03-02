3d4d03b8402862111ec49a8748289e6cc27dd02a
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2023-03-01 20:10:25 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const {GridFSBucket} = require('gridfs-stream');
+const Grid = require('gridfs-stream');
+const methodOver = require('method-override');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+const MongoUri = "mongodb+srv://shawntez32:Tezzyk32@cluster0.wpzbm.mongodb.net/?retryWrites=true&w=majority";
+const conn = mongoose.createConnection(MongoUri);
+
+let gfs, gridfsBucket;
+
+conn.once('open', () => {
+    // Init stream
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {bucketName: 'uploads'});
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+  });
+  
+  // Create storage engine
+  const storage = new GridFsStorage({
+    url: MongoUri,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  const upload = multer({ storage });
+  
+  app.get('/', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        res.render('index', { files: false });
+      } else {
+        files.map(file => {
+          if (
+            file.contentType === 'image/jpeg' ||
+            file.contentType === 'image/png'
+          ) {
+            file.isImage = true;
+          } else {
+            file.isImage = false;
+          }
+        });
+        res.render('index', { files: files });
+      }
+    });
+  });
+
+  app.post('/upload', upload.single('file'), (req, res) => {
+    // res.json({ file: req.file });
+    res.redirect('/');
+  });
+  
+  // @route GET /files
+  // @desc  Display all files in JSON
+  app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: 'No files exist'
+        });
+      }
+  
+      // Files exist
+      return res.json(files);
+    });
+  });
+  
+  // @route GET /files/:filename
+  // @desc  Display single file object
+  app.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+      // File exists
+      return res.json(file);
+    });
+  });
+  
+  // @route GET /image/:filename
+  // @desc Display Image
+  app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+  
+      // Check if image
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read output to browser
+        try{
+            const readstream = gridfsBucket.openDownloadStream(file._id);
+            readstream.pipe(res);
+        }catch{
+            console.log("No File");
+            res.redirect("/");
+        }
+        
+      } else {
+        res.status(404).json({
+          err: 'Not an image'
+        });
+      }
+    });
+  });
+  
+  app.delete('/files/:id', (req, res) => {
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+      if (err) {
+        return res.status(404).json({ err: err });
+      }
+  
+      res.redirect('/');
+    });
+  });
+  
+  const port = 5000;
+  
+  app.listen(port, () => console.log(`Server started on port ${port}`));
